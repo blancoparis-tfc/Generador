7ace549046923e4d721faa42904fe1c4d49f4698
blancoparis-tfc <blancoparis.tfc@gmail.com>
2016-04-19 00:15:33 +0200

    Poder escanear clases dentro de un paquete
    Closed #2
    
    He creaodo un método para poder escanear un fichero, dentro de un jar,
    par lo cual tenemos que utilizar un filesystem con el jar y dentro del
    fileSyustem recuperamos el ficher usando el separador en cuestión.

--- src/main/java/org/dbp/util/EscanearUtils.java
+++ src/main/java/org/dbp/util/EscanearUtils.java
@@ -3,14 +3,19 @@ package org.dbp.util;
 import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class EscanearUtils {
@@ -30,7 +35,7 @@ public class EscanearUtils {
 		final List<Class> valdev = new ArrayList<Class>();
 		List<URL> urls = recursosPaquete(paquete);
 		for(URL url:urls){
-			Path pathPaquete=Paths.get(url.toURI());
+			Path pathPaquete=resourceToPath(url);
 			try(Stream<Path> ficheros=Files.find(pathPaquete, 1, (matcher,options)->options.isRegularFile())){
 				List<String> strClases=ficheros
 						.map(path->construirPathClase(paquete, path))
@@ -43,6 +48,25 @@ public class EscanearUtils {
 		return valdev;
 	}
 
+	private Path resourceToPath(URL resource)
+			throws IOException,
+			       URISyntaxException {
+			    Objects.requireNonNull(resource, "Resource URL cannot be null");
+			    URI uri = resource.toURI();
+			    String scheme = uri.getScheme();
+			    if (scheme.equals("file")) {
+			        return Paths.get(uri);
+			    }
+			    if (!scheme.equals("jar")) {
+			        throw new IllegalArgumentException("Cannot convert to Path: " + uri);
+			    }
+			    String strUri = uri.toString();
+			    int separador = strUri.indexOf("!/");
+			    URI fileURI = URI.create(strUri.substring(0, separador));
+			    FileSystem fileSystemJar = FileSystems.newFileSystem(fileURI,Collections.<String, Object>emptyMap());
+			    return fileSystemJar.getPath(strUri.substring(separador + 2)); 
+			}
+	
 	private String construirPathClase(String paquete, Path path) {
 		return paquete.concat(".").concat(path.getFileName().toString().replace(".class", ""));
 	}
--- src/test/java/org/dbp/util/EscanerUtilTest.java
+++ src/test/java/org/dbp/util/EscanerUtilTest.java
@@ -1,12 +1,17 @@
 package org.dbp.util;
 
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+
+import junit.extensions.ActiveTestSuite;
+import junit.extensions.RepeatedTest;
+import junit.extensions.TestDecorator;
+import junit.extensions.TestSetup;
 
 import org.dbp.bean.MetaDatos;
 import org.junit.Test;
@@ -16,7 +21,7 @@ public class EscanerUtilTest {
 	@Test
 	public void testEscanearUnPaquete() throws ClassNotFoundException, IOException, URISyntaxException{
 		final List<Class> clases =EscanearUtils.instancia().escanearPaquete("org.dbp.bean");
-		assertThat("Esperamos encontrar esta clase",clases,contains(MetaDatos.class));
+		assertThat("Esperamos encontrar esta clase",clases,hasItem(MetaDatos.class));
 	}
 	
 	@Test
@@ -27,5 +32,14 @@ public class EscanerUtilTest {
 	@Test
 	public void testEscanearUnPaqueteDeUnaSolaPalabra() throws ClassNotFoundException, IOException, URISyntaxException{
 		assertEquals("No espera nada",0,EscanearUtils.instancia().escanearPaquete("dbp").size());
+	}
+	
+	@Test
+	public void testEscanearUnPaqueteJar() throws ClassNotFoundException, IOException, URISyntaxException{
+		List<Class> clases=EscanearUtils.instancia().escanearPaquete("junit.extensions");
+		assertThat("Esperamos encontrar esta clase",clases,hasItem(ActiveTestSuite.class));
+		assertThat("Esperamos encontrar esta clase",clases,hasItem(RepeatedTest.class));
+		assertThat("Esperamos encontrar esta clase",clases,hasItem(TestDecorator.class));
+		assertThat("Esperamos encontrar esta clase",clases,hasItem(TestSetup.class));
 	}
 }
